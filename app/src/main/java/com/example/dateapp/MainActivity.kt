b7c6445933229292a219a270541bc573aec77921
Spencer Parton <srparton1@buffs.wtamu.edu>
2022-02-13 00:18:31 -0600

    Added day of week functionality, formatted buttons, and padded output

--- app/src/main/java/com/example/dateapp/MainActivity.kt
+++ app/src/main/java/com/example/dateapp/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.dateapp
 import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.*
 import java.util.*
 
@@ -10,8 +11,17 @@ import java.util.*
    purpose: This app allows user to choose 3 dates and will then display
             those date by day of the week, year, month, day of the month
             format.
-   Work: Spencer -
+   Work: Spencer - Created project, added logic for finding DayOfWeek, padded
+         date and formatted layout.
          Braxton -
+   Learning: Spencer - In this project I learned more about API requirements. Some
+                       of the more modern methods of finding the date or the day of
+                       the week were not supported. IE LocalDate class. I also learned
+                       more about the Calendar Class and its extension the GregorianCalendar
+                       as I explored how to find the day of the week and to properly format
+                       our date output. I probably wound up coding about 40% of this project
+                       with Braxton coding the other 60%.
+             Braxton -
    app name: Date App
    minSDK 21, targetSDK 31
    author(s): Spencer Parton and Braxton Elrod
@@ -21,6 +31,8 @@ import java.util.*
 class MainActivity : AppCompatActivity() {
     var dateAmount = 0 //how many dates used
     var datesArray = arrayOfNulls<String>(3)
+    var weekDayName = arrayOf<String>("Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -33,23 +45,22 @@ class MainActivity : AppCompatActivity() {
                 return@setOnClickListener
             }
 
-            var remSting =(2 - dateAmount).toString() +  " Remaining clicks" // may not be correct amount
+            var remSting =(3 - dateAmount).toString() +  " Remaining Picks"
             Toast.makeText(this,remSting,Toast.LENGTH_SHORT).show()
             val cald = Calendar.getInstance()
             val year = cald.get(Calendar.YEAR)
             val month = cald.get(Calendar.MONTH)
             val day = cald.get(Calendar.DAY_OF_MONTH)
-
             val datePicker = DatePickerDialog(this,
-                {view, year,monthOfYear, dayOfMonth -> savePick(year, monthOfYear,dayOfMonth)},
+                {view, year, monthOfYear, dayOfMonth -> savePick(year, monthOfYear,dayOfMonth)},
                 year,
                 month,
-                day
+                day,
             )
+
             datePicker.show()
         }
         show.setOnClickListener{
-            var i = 0
             val a1 = findViewById<TextView>(R.id.activity1Date)
             val a2 = findViewById<TextView>(R.id.activity2Date)
             val a3 = findViewById<TextView>(R.id.activity3Date)
@@ -58,11 +69,25 @@ class MainActivity : AppCompatActivity() {
             a3.text = datesArray[2]
         }
     }
+
+//    private fun DatePickerDialog(mainActivity: MainActivity, i: Int, year: Int, i1: Int, day: Int, wkDay: Int): DatePickerDialog {
+//
+//    }
+
     private fun savePick(yy: Int, mm: Int, dd: Int){
-        var d = Date()
+
+        var mon = mm + 1
+        var date = GregorianCalendar(yy,mm,dd)
+        val wkDayInt = date.get(Calendar.DAY_OF_WEEK)
+        Log.d("wkDayInt","wkDayInt = "+wkDayInt)
+        val wkDay = weekDayName[wkDayInt-1]
+        Log.d("wkDay","wkDay: "+wkDay)
         if (dateAmount >=3)
             return
-        datesArray[dateAmount] = "Year ->" + yy + "Month-> " + mm + "Day ->" + dd
+//        val unformatted = "is on " + wkDay + ", " +yy + "/" + mon + "/" + dd
+//        val formatted =
+//        Log.d("formated","Formatted:"+formatted)
+        datesArray[dateAmount] = String.format("is on " + wkDay + ", " +"%02d" + "/" + "%02d" + "/" + "%02d",yy,mon,dd)
         dateAmount += 1
         return
     }
